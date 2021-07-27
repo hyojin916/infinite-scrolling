2d2e7d08ced88122ba7ab9d2bc5faec04b4bc841
hyojin <gywlsh274@gmail.com>
2021-07-27 15:58:37 +0900

    Add: nex branch

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useRef } from "react";
 
-function CommentBox({ comment, setPage }) {
+function CommentBox({ commentList }) {
   return (
     <div className="comment_box">
       <div className="comment_id">
         <div className="id_title">Comment</div>
-        <div className="id">{comment.id}</div>
+        <div className="id">{commentList.id}</div>
       </div>
       <div className="comment_email">
         <div className="email_title">Email</div>
-        <div className="email">{comment.email}</div>
+        <div className="email">{commentList.email}</div>
       </div>
       <div className="comment_main">
         <div className="comment_title">Comment</div>
-        <div className="comment_mseeage">{comment.body}</div>
+        <div className="comment_mseeage">{commentList.body}</div>
       </div>
     </div>
   );
